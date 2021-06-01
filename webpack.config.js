2da5b3b11280e97cb308fe9fb4eed3fc7525bb28
rafavramos <engenheirorafaramos@gmail.com>
2021-06-01 18:16:35 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const webpack = require('webpack')
+
+
+module.exports = {
+    entry: ['@babel/polyfill', path.resolve(__dirname, './src/main.js')],
+    output: {
+        path: path.resolve(__dirname, './public'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, './public'),
+        port: 3030
+    },
+    
+    module: {
+        rules: [
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        },
+        ],
+    },
+    resolve: {
+        alias: {
+          vue: 'vue/dist/vue.esm-bundler.js',
+        },
+      },
+      plugins: [
+        new webpack.DefinePlugin({
+          __VUE_OPTIONS_API__: true,
+          __VUE_PROD_DEVTOOLS__: false,
+        }),
+      ],
+}
